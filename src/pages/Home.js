5dcffb8dfd961bc21626b8021439ffa414cda547
yamathe <manuelsegmar@gmail.com>
2022-12-24 23:15:38 -0500

    Iinitial Template+ React Router & Sass & Styled Components

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PokemonBattle from "./PokemonsBattle";
+import PokemonInfoPage from "./PokemonInfoPage";
+
+export default function Home() {
+  return (
+    <>
+      <PokemonBattle></PokemonBattle>
+    </>
+  );
+}
